abd516557c807af82cb722c11f3aa55e1177b9bf
Henry <henry.fehrmann@vasslatam.com>
2021-01-19 09:48:32 -0300

    comienzo

---
+++ spring-react/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		String sql = "SELECT * FROM TRABAJADOR";
+
+		List<Trabajador> trabajadores = jdbcTemplate.query(sql,
+				BeanPropertyRowMapper.newInstance(Trabajador.class));
+		trabajadores.forEach(System.out :: println);
+	}
+
+}
