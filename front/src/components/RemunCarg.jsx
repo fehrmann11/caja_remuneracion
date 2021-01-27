47a9121431e26ca409cbb1d72b8cf524e38c0380
Henry <henry.fehrmann@vasslatam.com>
2021-01-27 15:51:21 -0300

    creación de header

--- front/src/components/RemunCarg.jsx
+++ front/src/components/RemunCarg.jsx
@@ -2,12 +2,14 @@ import React, {Component} from 'react'
 import LoginComponent from './login/LoginComponent'
 import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
 import WelcomeComponent from './welcome/WelcomeComponent';
+import HeaderComponent from './Header/HeaderComponent';
 
 class RemunCarg extends Component{
     render(){
         return(
             <div className="RemunCarg">
                 <Router>
+                    <HeaderComponent/>
                     <Switch>
                         <Route path="/" exact component={LoginComponent}/>
                         <Route path="/welcome" component={WelcomeComponent}/>
