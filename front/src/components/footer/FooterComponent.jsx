f270703e92b83071d563040b282f5e153ed72ae6
Henry <henry.fehrmann@vasslatam.com>
2021-01-28 09:28:38 -0300

    termino de footer

--- front/src/components/footer/FooterComponent.jsx
+++ front/src/components/footer/FooterComponent.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './FooterComponent.css'
 //import { Modal, Button} from 'react-bootstrap'
 class FooterComponent extends Component {
     render() {
         return (
             <footer className="footer">
-                <span className="text-muted">Todos los derechos reserados @Henry Fehrmann</span>
+                <span className="text-muted">Copyright © 2021 Todos los derechos reservados</span>
             </footer>
         )
     }
