029ff503298fce75bb8eb8bdc8d2911af8677945
Jose Hernandez <jose.hernandez@vasslatam.com>
2021-02-04 06:46:35 -0300

    Organize imports

--- demo/src/main/java/cl/vass/practica/springreact/repository/EmpleadorRepository.java
+++ demo/src/main/java/cl/vass/practica/springreact/repository/EmpleadorRepository.java
@@ -1,6 +1,5 @@
 package cl.vass.practica.springreact.repository;
 
-import cl.vass.practica.springreact.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cl.vass.practica.springreact.model.Empleador;
