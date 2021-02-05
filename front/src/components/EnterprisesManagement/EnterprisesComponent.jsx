7599b3e708f1adb58be2f84b4b2d6c05246ccc8d
Henry <henry.fehrmann@vasslatam.com>
2021-02-05 12:36:34 -0300

    errorComponent 404 creado

---
+++ front/src/components/EnterprisesManagement/EnterprisesComponent.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class EnterprisesComponent extends Component{
+    constructor(props){
+        super(props)
+
+        this.GetEnterprises = this.GetEnterprises.bind(this);
+    }
+
+    GetEnterprises(){
+        
+    }
+
+}
+
+export default EnterprisesComponent;
