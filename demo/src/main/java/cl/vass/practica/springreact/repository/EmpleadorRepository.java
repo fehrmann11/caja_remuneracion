749b928d95bbc69d93bbfdb95e64e9567801fb17
Jose Hernandez <jose.hernandez@vasslatam.com>
2021-02-10 01:31:56 -0300

    elimino herencia innecesaria

--- demo/src/main/java/cl/vass/practica/springreact/repository/EmpleadorRepository.java
+++ demo/src/main/java/cl/vass/practica/springreact/repository/EmpleadorRepository.java
@@ -1,14 +1,13 @@
 package cl.vass.practica.springreact.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import cl.vass.practica.springreact.model.Empleador;
 
 import java.util.List;
 import java.util.Map;
 
-public interface EmpleadorRepository extends JpaRepository<Empleador, String>, JpaSpecificationExecutor<Empleador> {
+public interface EmpleadorRepository extends JpaRepository<Empleador, String>{
     //Buscar empleador por nombre
     public List<Empleador> getData(Map<String, Object> conditions);
 }
