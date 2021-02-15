21a344535ccdca018eea9cf3070247ae75b07a20
henryfc7 <henryfc7@gmail.com>
2021-02-15 16:20:07 -0300

    pantalla inicial arreglada

--- front/src/components/initial/InitialScreen.jsx
+++ front/src/components/initial/InitialScreen.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FooterComponent extends Component {
-    render() {
-        return (
-            <div className="video">
-                <video muted loop autoPlay style={{position:'absolute',
-                                                    height:'85%',
-                                                    width:'100%'}}
-                id="myVideo" controls poster="true">
-                    <source type="video/mp4" src="https://vasscompany.com/wp-content/uploads/2021/01/Videos-home-VASS.mp4"/>
-                </video>
-            </div>
-        )
-    }
+const InitialScreen = ( ) =>{
+    return (
+        <div className="video">
+            <video muted loop autoPlay style={{position:'absolute',
+                                                height:'85%',
+                                                width:'100%'}}
+            id="myVideo" controls poster="true">
+                <source type="video/mp4" src="https://vasscompany.com/wp-content/uploads/2021/01/Videos-home-VASS.mp4"/>
+            </video>
+        </div>
+    )
 }
 
-export default FooterComponent;
+export default InitialScreen;
 
