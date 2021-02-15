feed74ea5cefae9f6e5f7b41d871fc3a9faf8466
henryfc7 <henryfc7@gmail.com>
2021-02-15 17:20:53 -0300

    login actualizado con hooks

--- front/src/components/login/LoginComponent.jsx
+++ front/src/components/login/LoginComponent.jsx
@@ -1,89 +1,80 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import './LoginComponent.css'
 import SecurityService from '../../api/SecurityService'
+import {useHistory } from 'react-router-dom';
 
 
+const LoginComponent = () => {
+    const [userName, setUserName] = useState("ingrese su usuario");
+    const [password, setPassword] = useState("ingrese su contraseña");
+    const [hasLoginFailed, setHasLoginFailed] = useState(false);
+    let history = useHistory();
 
-class LoginComponent extends Component {
-
-    //inicialización de variables
-    constructor(props) {
-        super(props)
-        this.state = {
-            userName: "ingrese su usuario",
-            password: "ingrese su contraseña",
-            hasLoginFailed: false
-        }
-
-        //bindeo de las funciones
-        this.handleChange = this.handleChange.bind(this)
-        this.loginClicked = this.loginClicked.bind(this)
-
-    }
 
     /*Está función se lanza al momento de dar clic en el botón la cual hace una promesa
     a través de SecurityService y obtiene un token, una vez obtenido el token se va a
     la pantalla management (que es welcomeComponent)*/
-    loginClicked() {
-       SecurityService.AuthenticationService(this.state.userName,this.state.password)
-            .then((response)=>{
-            SecurityService.registerSuccessfulLoginForJwt(this.state.userName,response.data.jwt)
-            this.props.history.push(`/management`)
-        }).catch(()=>{
-            this.setState({hasLoginFailed:true})
-        })
+    const loginClicked = () => {
+        SecurityService.AuthenticationService(userName, password)
+            .then((response) => {
+                SecurityService.registerSuccessfulLoginForJwt(userName, response.data.jwt)
+                history.push(`/management`)
+            }).catch(() => {
+                setHasLoginFailed(true);
+            })
     }
-
 
     //Esta función guarda el usuario y contraseña en las variables userName y password
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value 
-        })
-
+    const handleChangeUsername = (event) => {
+        console.log(userName)
+        setUserName(event.target.value);
     }
 
+    const handleChangePassword = (event) =>{
+        console.log(password)
+        setPassword(event.target.value)
+    }
 
+    //Formulario
+    return (
+        <div className="wrapper fadeInDown">
+            <div id="formContent">
+                {/*icono de la empresa */}
+                <div className="fadeIn first">
+                    <img src="https://ii.ct-stc.com/4/logos/empresas/2018/02/07/vass-chile-CCFC14226ACF2C23151119258thumbnail.jpg"
+                        alt="logo"
+                        id="icon" />
+                </div>
+                {/*Formulario */}
+                <div >
 
-    render() {
-        return (
-            <div className="wrapper fadeInDown">
-                <div id="formContent">
-                    {/*icono de la empresa */}
-                    <div className="fadeIn first">
-                        <img src="https://ii.ct-stc.com/4/logos/empresas/2018/02/07/vass-chile-CCFC14226ACF2C23151119258thumbnail.jpg"
-                            alt="logo"
-                            id="icon" />
-                    </div>
-                    {/*Formulario */}
-                    <div >
+                    <input type="text"
+                        id="login"
+                        className="fadeIn second"
+                        name="userName"
+                        placeholder={userName}
+                        onChange={handleChangeUsername} />
 
-                        <input type="text"
-                            id="login"
-                            className="fadeIn second"
-                            name="userName"
-                            placeholder={this.state.userName}
-                            onChange={this.handleChange} />
+                    <input type="password"
+                        id="password"
+                        className="fadeIn third"
+                        name="password"
+                        placeholder={password}
+                        onChange={handleChangePassword} />
 
-                        <input type="password"
-                            id="password"
-                            className="fadeIn third"
-                            name="password"
-                            placeholder={this.state.password}
-                            onChange={this.handleChange} />
-
-                        <button className="btn btn-primary" onClick={this.loginClicked}>iniciar sesión</button>
-
-                    </div>
-                    {this.state.hasLoginFailed && <div className="alert alert-warning" role="alert">
-                        Tu usuario o contraseña son inválidos
-                    </div>}
-
+                    <button className="btn btn-primary" onClick={loginClicked}>iniciar sesión</button>
 
                 </div>
+                {hasLoginFailed && <div className="alert alert-warning" role="alert">
+                    Tu usuario o contraseña son inválidos
+                </div>}
+
+
             </div>
-        )
-    }
+        </div>
+    )
+
+
 }
 
 export default LoginComponent;
