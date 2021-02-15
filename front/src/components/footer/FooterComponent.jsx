fff919ee0b6e05d7121f76333c6feed438befffc
henryfc7 <henryfc7@gmail.com>
2021-02-15 16:18:02 -0300

    footer arreglado

--- front/src/components/footer/FooterComponent.jsx
+++ front/src/components/footer/FooterComponent.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+
 import './FooterComponent.css'
-class FooterComponent extends Component {
-    render() {
-        return (
-            <footer className="footer">
-                <span className="text-muted">Copyright © 2021 Todos los derechos reservados</span>
-            </footer>
-        )
-    }
+
+const FooterComponent = () => {
+    return (
+        <footer className="footer">
+            <span className="text-muted">Copyright © 2021 Todos los derechos reservados</span>
+        </footer>
+    )
 }
+
 
 export default FooterComponent;
