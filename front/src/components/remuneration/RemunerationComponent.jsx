c9a462b5fa74d05ef9b7fedf8b59e954f1a34584
henryfc7 <henryfc7@gmail.com>
2021-02-23 16:22:50 -0300

    probando una librería

--- front/src/components/remuneration/RemunerationComponent.jsx
+++ front/src/components/remuneration/RemunerationComponent.jsx
@@ -1,39 +1,45 @@
 import Title from '../reuseComponent/Title';
 import useGetdata from '../hooks/useGetdata';
-import { Table} from 'react-bootstrap';
-import verificador  from 'verificador-rut';
+import {  Table } from 'react-bootstrap';
+import verificador from 'verificador-rut';
 import EnterprisesService from '../../api/EnterprisesService';
-import {useState} from 'react';
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { Formik,Form, Field } from 'formik';
+//import Select from 'react-select';
 
 
 const RemunerationComponent = () => {
-    const [cargas,setCargas] = useState([]);
-    const [empleadores,setEmpleadores] = useState([]);
+    const [cargas, setCargas] = useState([]);
+    const [empleadores, setEmpleadores] = useState([]);
     var [estado, setEstado] = useState(false);
-    const [periodo,setPeriodo] = useState([]);
-    const [rut,setRut] = useState('');
+    const [periodos, setPeriodos] = useState([]);
+    //formulario
+    const [rut, setRut] = useState('');
+    const [periodo] = useState('');
+    const [empleador] = useState('');
+    const [carga] = useState('');
 
     let history = useHistory();
 
     const API_TRABAJADOR = '/private/trabajador';
     const trabajadores = useGetdata(API_TRABAJADOR);
-    
-    
-    
+
+
+
     //Esta funcionalidad lo que busca es traer las cargas, y los empleadores.
-    const DetailInput = async(rut) =>{
+    const DetailInput = async (rut) => {
         setEstado(true);
         setRut(rut);
-        
+
         try {
             const response = await EnterprisesService.returnGet(`/private/trabajador/${rut}/cargas`);
             const response_empleador = await EnterprisesService.returnGet(`/private/trabajador/${rut}/empleador`);
-            const response_periodo = await EnterprisesService.returnGet('/private/periodo');
+            const response_periodos = await EnterprisesService.returnGet('/private/periodo');
 
             setCargas(response.data);
             setEmpleadores(response_empleador.data);
-            setPeriodo(response_periodo.data);
+            setPeriodos(response_periodos.data);
 
         } catch (error) {
             console.log(error);
@@ -41,13 +47,13 @@ const RemunerationComponent = () => {
 
     }
 
-    const Consultar = (id) =>{
-        console.log(id)
-        history.push(`/remuneration/${id}`)
-    //     //función que actualiza una empresa
-    // const updateEnterprise = (id) => {
-    //     history.push(`/enterprisesManagement/${id}`)
+    // const Consultar = (id) => {
+    //     console.log(id)
+    //     history.push(`/remuneration/${id}`)
     // }
+
+    const onSubmit = (values) =>{
+        console.log(values);
     }
 
 
@@ -56,7 +62,7 @@ const RemunerationComponent = () => {
         <div className="container">
             <Title titulo="Remuneraciones" />
             {/*información */}
-            
+
             <Table striped bordered hover size="sm">
                 <thead>
                     <tr id="headerInfo">
@@ -64,44 +70,56 @@ const RemunerationComponent = () => {
                         <th>Rut</th>
                     </tr>
                 </thead>
-                
+
                 {<tbody>
                     {
                         trabajadores.map(
                             trabajador =>
                                 //let rutView = trabajador.rut;
-                                <tr id="info" onClick={()=>DetailInput(trabajador.rut)} key={trabajador.rut}>
+                                <tr id="info" onClick={() => DetailInput(trabajador.rut)} key={trabajador.rut}>
                                     <td >{trabajador.nombres} {trabajador.apellidoPaterno} {trabajador.apellidoMaterno}</td>
-                                    <td >{trabajador.rut.slice(0,trabajador.rut.length-1)+"-"+verificador(trabajador.rut.slice(0,trabajador.rut.length-1))}</td>
+                                    <td >{trabajador.rut.slice(0, trabajador.rut.length - 1) + "-" + verificador(trabajador.rut.slice(0, trabajador.rut.length - 1))}</td>
                                 </tr>
-                            )
+                        )
 
-                        }
-                </tbody> }
+                    }
+                </tbody>}
             </Table >
-            {(estado&&cargas.length!==0&&
-            <div>
-                no tienes cargas
-            </div>) && 
-            <div>
-                <select>
-                    {empleadores.map(empleador=>(
-                        <option key={empleador.rut}>{empleador.razonSocial}</option>
-                    ))}    
-                </select>
-                <select>
-                {cargas.map(carga=>(
-                        <option key={carga.rut}>{carga.nombres}</option>
-                        
-                    ))}  
-                </select>
-                <select>
-                {periodo.map(per=>(
-                    <option key={per.rut}>{per.nombre}</option>
-                ))}
-                </select>
-                <button onClick={()=>Consultar(rut)}>Consultar</button>                    
-            </div>
+            {(estado && cargas.length !== 0 &&
+                <div>
+                    no tienes cargas
+            </div>) &&
+                <Formik
+                    initialValues={{
+                        rut,
+                        empleador,
+                        carga,
+                        periodo
+                    }}
+                    onSubmit={onSubmit}
+                >{() => (
+                    <Form>
+                        <Title titulo={"Consulta de remuneración"} />
+                        <Field as="select" name="empleador">
+                            {empleadores.map(emp => (
+                                <option key={emp.rut}>{emp.razonSocial}-{emp.rut}</option>
+                            ))}
+                        </Field>
+                        <Field as="select" name="carga">
+                            {cargas.map(car => (
+                                <option key={car.rut}>{car.nombres}-{car.rut}</option>
+
+                            ))}
+                        </Field>
+                        <Field as="select" name="periodo">
+                            {periodos.map(per => (
+                                <option key={per.id}>{per.nombre}-{per.id}</option>
+                                
+                            ))}
+                        </Field>
+                        <button type="submit">Consultar</button>
+                    </Form>)}
+                </Formik>
             }
         </div>
     )
